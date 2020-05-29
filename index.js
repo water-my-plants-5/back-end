4f431c00220b34d71370cb0fa8263ec66c0c50ac
Brandon Green <jailhousejordans@gmail.com>
2020-05-28 20:52:09 -0500

    added fix for PUT request

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ server.use(helmet())
 server.use(express.json())
 server.use(cookieParser())
 
+server.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+
 server.use("/auth", authRouter)
 server.use("/users", usersRouter)
 server.use("/plants", plantsRouter);
